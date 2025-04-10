6d1be2b05b0b5ecef04dbe31fe5ff58a5312e6fb
Mayanksankhla2003 <sankhlamayank2003@gmail.com>
2025-04-10 16:54:21 +0530

    Initialize student job tracker project

---
+++ client/src/components/FilterBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const FilterBar = () => {
+    return (
+        <div className="w-[40%] mx-15 flex justify-around border-2 border-gray-300 rounded-lg p-3 bg-white shadow-md">
+            <select className="w-[40%] border-2 border-gray-200 rounded-lg focus:outline-none focus:border-2 focus:border-blue-500 p-2">
+                <option value="applied">Applied</option>
+                <option value="interview">Interview</option>
+                <option value="offer">Offer</option>
+                <option value="rejected">Rejected</option>
+            </select>
+            <input
+                type="date"
+                className="w-[40%] border-2 border-gray-200 rounded-lg focus:outline-none focus:border-2 focus:border-blue-500 p-2"
+            />
+        </div>
+    );
+};
+
+export default FilterBar;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import FilterBar from "../components/FilterBar";
+
+const Home = () => {
+    return (
+        <div className="w-full h-full">
+            <h1 className="text-3xl font-bold p-15">Student Job Tracker</h1>
+            <FilterBar />
+            <button></button>
+        </div>
+    );
+};
+
+export default Home;
